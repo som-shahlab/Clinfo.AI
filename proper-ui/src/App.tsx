9b680a9cbaa5d048d6dff3ec4f8fa56eff9f6eab
Truong Nhat Anh <truongnhatanhw@gmail.com>
2024-08-09 14:46:03 +0700

    feat: add dark mode

--- proper-ui/src/App.tsx
+++ proper-ui/src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
+import { ThemeProvider } from "./components/theme-provider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <ModeToggle />
+        <Button>Hello world</Button>
+      </div>
+    ),
+  },
+]);
 
 function App() {
   return (
     <>
-    
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
